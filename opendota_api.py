3edacc8fee5d0db6199fee51b45b95caf995f8c1
sun <weixuansun7@gmail.com>
2018-06-01 16:29:29 +1000

    extract result and team data from raw csv data and put them into array

--- opendota_api.py
+++ opendota_api.py
@@ -7,6 +7,10 @@ import time
 import urllib3
 
 
+
+
+
+
 #url = ('https://api.opendota.com/api/matches/271145478?api_key=3ea68b18-147a-4d37-9d13-68e24a4fd033')
 #r = requests.get(url)
 
@@ -47,13 +51,25 @@ class data_process(object):
             f = csv.reader(csv_file)
             f = list(f)
             print(len(f))
-            data_list = []
-
+            data_matrix = np.zeros([int((len(f)-1)/2),11])
             for i in range(2,len(f),2):
-                data_list.append(f[i][2]+f[i][12])
-            # print(data_list)
-            print(len(data_list))
-        return (data_list)
+                if f[i][2] == 'TRUE':
+                    data_matrix[int((i/2)-1),0] = 1
+                else:
+                    data_matrix[(int(i/2)-1),0] = 0
+                radiant_team = f[i][12]
+                dire_team = f[i][13]
+                radiant_team = radiant_team.split(',',4)
+                dire_team = dire_team.split(',',4)
+                # print(radiant_team)
+                # print(dire_team)
+                data_matrix[(int(i / 2) - 1), 1:6] = radiant_team[0:5]
+                data_matrix[(int(i / 2) - 1), 6:11] = dire_team[0:5]
+                # print(radiant_team)
+            print(data_matrix)
+            print(np.shape(data_matrix))
+        return (data_matrix)
+
 
 
 
@@ -70,23 +86,36 @@ if __name__ == '__main__':
     data_process_1 = data_process()
 
     #  collect data
-    match_data = []
-    for step in range(300):
-        print(step)
-        raw_data = data_process_1.get_data()
-        raw_data = json.loads(raw_data.content.decode('utf-8'))
-        for item in raw_data:
-            match_data.append(item)
-        time.sleep(60)
-    data_process_1.save_data(match_data)
+    # match_data = []
+    # for step in range(300):
+    #     print(step)
+    #     raw_data = data_process_1.get_data()
+    #     raw_data = json.loads(raw_data.content.decode('utf-8'))
+    #     for item in raw_data:
+    #         match_data.append(item)
+    #     time.sleep(60)
+    # data_process_1.save_data(match_data)
 
 
     # data_process_1.get_hero_data(1)
 
-    # data_list = data_process_1.process_data('D:/Noobdota2predictor/data.csv')
+    data_list = data_process_1.process_data('/home/sun/dota2predictor/Noobdota2predictor/data.csv')
     # print(data_list)
+    #
 
-
+    ###caogaozhi
+    # a = np.array([[1,2,3],[2,3,4]])
+    # print(a[0:100,1])
+    # print(a)
+    # print(a[1,2])
+    # print(np.zeros([5,3]))
+    # b = np.zeros(5)
+    # print(b)
+    # a = [11,12,12,1,12,33,4,5]
+    # print(a[1:5])
+    # b[0:5] = a[1:6]
+    # print(b)
+    #
 
 
 
