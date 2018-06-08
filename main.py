86951b620d3a18a3f93c46dcc93fd6c39cf46561
weixuansun <weixuansun7@gmail.com>
2018-06-08 14:25:53 +1000

    start training!

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class data_process(object):
         # print(heroes.content)
         # print(heroes.content.decode('UTF-8'))
         heroes_dict = json.loads(heroes.content.decode('utf-8'))
-        print(heroes_dict)
+        # print(heroes_dict)
         # print(heroes_dict[hero_id-1]['localized_name'])
         # for i in range(115):
         #     print(heroes_dict[i])
@@ -46,7 +46,7 @@ class data_process(object):
         with open(filename) as csv_file:
             f = csv.reader(csv_file)
             f = list(f)
-            print(len(f))
+            # print(len(f))
             data_matrix = np.zeros([int((len(f)-1)/2),11])
             for i in range(2,len(f),2):
                 data_matrix[int((i / 2) - 1), 0] = 1 if f[i][2] == 'TRUE' else 0
@@ -96,13 +96,11 @@ class data_process(object):
 
 # class train(object):
 
-def train(heros_features, results_data):
+def train(heros_features, results_data,test_heros_features, test_result_data):
     learning_rate = 0.001
-    training_epochs = 10
+    training_epochs = 100
     batch_size = 100
     display_step = 1
-
-
     # match_data = np.asanyarray(match_data,np.float32)
 
     x = tf.placeholder(tf.float32,[None, 70])
@@ -113,14 +111,15 @@ def train(heros_features, results_data):
     features, labels = iter.get_next()
     # print(dataset)
 
-    W = tf.Variable(tf.zeros([70,1]))
-    b = tf.Variable(tf.zeros([1]))
+
+    W = tf.Variable(tf.random_normal([70,1]))
+    b = tf.Variable(tf.random_normal([1]))
     prediction = tf.nn.sigmoid(tf.matmul(x,W)+b)
     # cost
 
-    loss = tf.reduce_min(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels=y))
+    loss = tf.reduce_min(tf.nn.sigmoid_cross_entropy_with_logits(logits=prediction,labels=y))
     #
-    train = tf.train.GradientDescentOptimizer(0.2).minimize(loss)
+    train = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
     #
     # save result to a boolean list
     # correct_prediction = tf.equal(y, tf.round(prediction))
@@ -129,16 +128,30 @@ def train(heros_features, results_data):
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        sess.run(iter.initializer, feed_dict={x: heros_features, y: results_data})
+        # sess.run(iter.initializer, feed_dict={x: heros_features, y: results_data})
         for epoch in range(training_epochs):
             total_batch = int(len(results_data)/batch_size)
-            print(total_batch)
-            x_batches = np.array_split(heros_features,total_batch)
-            y_batches = np.array_split(results_data,total_batch)
+            x_batches = np.array_split(heros_features, total_batch)
+            y_batches = np.array_split(results_data, total_batch)
+            # print(total_batch)
+            # print(len(x_batches), len(y_batches))
             for i in range(total_batch):
-                print(i)
+                avg_cost = 0.
                 batch_x, batch_y = x_batches[i],y_batches[i]
-                sess.run(train,feed_dict={x:batch_x, y:batch_y})
+                # print(batch_x)
+                # print(batch_y)
+                pre = sess.run(prediction, feed_dict={x: batch_x, y: batch_y})
+                # print(pre)
+                # print(batch_x.shape,batch_y.shape)
+                _, c = sess.run([train, loss],feed_dict={x:batch_x, y:batch_y})
+                # print(c)
+                avg_cost += c / total_batch
+
+            if epoch%display_step == 0 :
+                print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
+        print('training finished')
+
+
             # if epoch%display_step == 0 :
             #     print(...)
                 # acc = sess.run(accuracy, feed_dict={x:features,y:labels})
@@ -166,17 +179,22 @@ if __name__ == '__main__':
     heros_id = np.arange(1, 121)
     heros_id_matrix = data_process_1.vec_bin_array(heros_id,7)
     heros_dict = dict(zip(heros_id,heros_id_matrix))
-    print(heros_dict)
+    # print(heros_dict)
     heros_data, results_data = data_process_1.process_data('D:/Noobdota2predictor/data.csv')
-    results_data = np.transpose(results_data)
-    print(results_data.shape)
+    test_heros_data, test_results_data = data_process_1.process_data('D:/Noobdota2predictor/data_2.csv')
+    # results_data = np.transpose(results_data)
+    # print(results_data.shape)
+    test_results_data = np.reshape(test_results_data,[30000,1])
     results_data = np.reshape(results_data,[30000,1])
-    print(results_data)
-    # print(heros_data.shape[1])
+    # print(results_data)
+    print(heros_data.shape)
     #  todo: sort heros of both team by positions: carry, mid, initiate, support.
     heros_features = data_process_1.map_heros_data_matrix(heros_data,heros_dict)
-    print(heros_features)
-    train(heros_features,results_data)
+    test_heros_features = data_process_1.map_heros_data_matrix(test_heros_data,heros_dict)
+    print(heros_features.shape)
+    train(heros_features,results_data,test_heros_features,test_results_data)
+
+
 
 
 
--- test.py
+++ test.py
@@ -10,17 +10,21 @@ import  tensorflow as tf
 # print(a[1,0:3])
 # for i in range(10):
 #     print(i)
-a = tf.Variable(tf.ones([100,10]))
-b = tf.Variable(tf.ones([10,100]))
-c = tf.matmul(a,b)
-d = tf.nn.sigmoid(tf.matmul(a,b))
-e = tf.round(d)
-
-
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    e1 = sess.run(c)
-    e2 = sess.run(d)
-    e3 = sess.run(e)
-    print(e1.shape)
-    print(e3)
+# a = tf.Variable(tf.ones([100,10]))
+# b = tf.Variable(tf.ones([10,100]))
+# c = tf.matmul(a,b)
+# d = tf.nn.sigmoid(tf.matmul(a,b))
+# e = tf.round(d)
+#
+#
+# with tf.Session() as sess:
+#     sess.run(tf.global_variables_initializer())
+#     e1 = sess.run(c)
+#     e2 = sess.run(d)
+#     e3 = sess.run(e)
+#     print(e1.shape)
+#     print(e3)
+#
+a = np.zeros([30000,1])
+b = np.array_split(a,100)
+print(b[1])
